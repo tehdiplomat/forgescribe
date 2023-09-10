d7b98748fc9dfe64a8173287f996a0b35cd378a5
Chris H <zenchristo@gmail.com>
2023-09-10 16:14:32 -0400

    Update forgeScribe6.0.sh

--- scripts/forgeScribe6.0.sh
+++ scripts/forgeScribe6.0.sh
@@ -6,7 +6,7 @@ head -31 /NMT/2020train/forge.test.or > /tmp/tokenedoracle
 echo $stringin | /NMT/scripts/tokenizer >> /tmp/tokenedoracle
 currentdir=`pwd`
 cd /NMT/
-python -m nmt.nmt --ckpt=${model}/best_bleu/translate.ckpt-34000 --out_dir=${model} --src=or --tgt=sc --inference_input_file=/tmp/tokenedoracle --inference_output_file=/tmp/tokenedscript --vocab_prefix=./${model}/forge.vocab > /dev/null 2>&1
+python -m nmt.nmt --ckpt=${model}/best_bleu/translate.ckpt-34000 --out_dir=${model} --src=or --tgt=sc --inference_input_file=/tmp/tokenedoracle --inference_output_file=/tmp/tokenedscript --vocab_prefix=./${model}/forge.vocab
 #python -m nmt.nmt --out_dir=./nmt_model_gnmt/best_bleu/ --src=or --tgt=sc --tgt_max_len=512 --num_units=512 --hparams_path=/NMT/nmt/standard_hparams/forge.json --inference_input_file=/tmp/tokenedoracle --inference_output_file=/tmp/tokenedscript --vocab_prefix=./nmt_model_gnmt/forge.vocab > /dev/null 2>&1
 cd $currentdir
 tail -1 /tmp/tokenedscript | /NMT/scripts/detokenizer
